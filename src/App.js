88ffaaf74858c8730373b72788dad0ffd1a76d5e
Ishan-Chanuka <ishanchanuka4@gmail.com>
2022-11-22 20:32:32 +0530

    cleand repo

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="App-logo">Hi Jola</h1>
+        <h2>
+          Welcome to CS Dream House
+        </h2>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site will get online soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
