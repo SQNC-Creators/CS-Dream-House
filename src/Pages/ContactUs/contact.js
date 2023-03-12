5b146c82f9a8effffc8a0f93b6c85ce3629f2b41
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-03-12 13:57:18 +0530

    Contact page completed

--- src/Pages/ContactUs/contact.js
+++ src/Pages/ContactUs/contact.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import {
+  MDBInput,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import './contact.css';
+import contactImg from './../../Assets/Images/contact-img.png';
 
 function contact() {
   return (
-    <div>contact</div>
+    <MDBContainer className='contact-main'>
+      <MDBRow>
+        <MDBCol className='contact-f'>
+          <form>
+            <h2 className='contact-formH'>Contact Us</h2>
+            <MDBRow>
+              <label className='inp-lable'>Your Name</label>
+              <input className='inp-contact' type='text' id='form1Example1' />
+            </MDBRow>
+            <MDBRow>
+              <label className='inp-lable'>Email</label>
+              <input className='inp-contact' type='text' id='form1Example1' />
+            </MDBRow>
+            <MDBRow>
+              <label className='inp-lable'>Your Message</label>
+              <textarea className='inp-contact' type='text' id='form1Example1' placeholder='Type your message here...'></textarea>
+            </MDBRow>
+
+            <button type='submit' block className='cont-btn'>
+              Contact Us
+            </button>
+          </form>
+
+        </MDBCol>
+        <MDBCol className='img-col-contact'>
+          <div className='contact-imgdiv'><img src={contactImg} alt='' className='contatcIm'/></div>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   )
 }
 
